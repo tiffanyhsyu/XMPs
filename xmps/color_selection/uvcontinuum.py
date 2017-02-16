c112f140d7907fc6133001bc65ba88117ccf2d5e
ktirimba <ktirimba@ucsc.edu>
2017-02-15 22:10:54 -0800

    Update uvcontinuum.py

--- xmps/color_selection/uvcontinuum.py
+++ xmps/color_selection/uvcontinuum.py
@@ -1,1 +1,13 @@
+from astropy.io import fits
 
+hdu=fits.open(file)
+data=hdu[1].data
+wave=10**data['loglam']
+flux=data['flux']*1e-17
+
+const=np.median(flux[0:50])/((wave[0])**(-2.3))
+#print const
+UV_wave=np.arange(2000,wave[0],0.5)
+UV_normalized_flux=(const*UV_wave**-2.3)
+wave=np.concatenate((UV_wave,wave),axis=0)
+flux=np.concatenate((UV_normalized_flux,flux),axis=0)
